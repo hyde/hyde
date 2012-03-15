76c60c6e33d6f0cb751de83a6b5497e351073d89
Grygoriy Fuchedzhy <grygoriy.fuchedzhy@gmail.com>
2012-03-15 20:05:15 +0200

    added smart language plugin

--- hyde/ext/plugins/languages.py
+++ hyde/ext/plugins/languages.py
@@ -5,6 +5,15 @@ Contains classes to help manage multi-language pages.
 
 from hyde.plugin import Plugin
 
+from hyde.fs import File, Folder
+from hyde.model import Expando
+from hyde.plugin import Plugin
+import hyde.site
+
+from functools import wraps
+
+import os
+
 class LanguagePlugin(Plugin):
     """
     Each page should be tagged with a language using `language` meta
@@ -42,6 +51,12 @@ class LanguagePlugin(Plugin):
         """
         Initialize plugin. Build the language tree for each node
         """
+
+        if hasattr(self.site.config, 'languages'):
+            settings = self.site.config.languages
+        else:
+            settings = None
+
         # Build association between UUID and list of resources
         for node in self.site.content.walk():
             for resource in node.resources:
@@ -49,7 +64,19 @@ class LanguagePlugin(Plugin):
                     uuid = resource.meta.uuid
                     language = resource.meta.language
                 except AttributeError:
-                    continue
+                    try:
+                        i = resource.name.index('_')
+                    except ValueError:
+                        continue
+                    language = resource.name[:i]
+                    name_without_prefix = resource.name[i+1:]
+                    if not len(language) == 2 or not language.isalpha():
+                        continue
+                    if hasattr(settings, 'modify_path') and settings.modify_path:
+                        resource.set_relative_deploy_path(language + os.sep + (resource.node.relative_path + os.sep if resource.node.relative_path else '') + name_without_prefix)
+                    uuid = (resource.node.relative_path + os.sep if resource.node.relative_path else '') + name_without_prefix
+                    resource.meta.language = language
+                    resource.meta.uuid = uuid
                 if uuid not in self.languages:
                     self.languages[uuid] = []
                 self.languages[uuid].append(resource)
@@ -65,3 +92,22 @@ class LanguagePlugin(Plugin):
                     "Adding translations for resource [%s] from %s to %s" % (resource,
                                                                              language,
                                                                              translations))
+
+        def get_url(urlgetter):
+            @wraps(urlgetter)
+            def wrapper(context, path, safe=None):
+                f = File(path)
+                if isinstance(context, hyde.site.Site):
+                    return urlgetter(context, path, safe)
+                res = context['resource']
+                try:
+                    localized_path = res.meta.language + os.sep + path
+                except AttributeError:
+                    return urlgetter(context, path, safe)
+                res_localized = res.site.content.resource_from_relative_deploy_path(localized_path)
+                return urlgetter(context, localized_path if res_localized else path, safe)
+            return wrapper
+
+        if hasattr(settings, 'modify_path') and settings.modify_path:
+            hyde.site.content_url = get_url(hyde.site.content_url)
+            hyde.site.media_url = get_url(hyde.site.media_url)
