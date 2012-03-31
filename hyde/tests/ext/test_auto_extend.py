d3a3e0a057709a6a8e71c51d9082ecaa3a7b7c13
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2012-03-31 19:29:23 -0400

    Move the AutoExtend plugin into the meta module.

--- hyde/tests/ext/test_auto_extend.py
+++ hyde/tests/ext/test_auto_extend.py
@@ -42,7 +42,7 @@ class TestAutoExtend(object):
     def test_can_auto_extend(self):
         s = Site(TEST_SITE)
         s.config.plugins = ['hyde.ext.plugins.meta.MetaPlugin',
-                            'hyde.ext.plugins.auto_extend.AutoExtendPlugin',
+                            'hyde.ext.plugins.meta.AutoExtendPlugin',
                             'hyde.ext.plugins.text.BlockdownPlugin']
         txt ="This template tests to make sure blocks can be replaced with markdownish syntax."
         templ = """
@@ -59,7 +59,7 @@ extends: base.html
     def test_can_auto_extend_with_default_blocks(self):
         s = Site(TEST_SITE)
         s.config.plugins = ['hyde.ext.plugins.meta.MetaPlugin',
-                            'hyde.ext.plugins.auto_extend.AutoExtendPlugin',
+                            'hyde.ext.plugins.meta.AutoExtendPlugin',
                             'hyde.ext.plugins.text.BlockdownPlugin']
         txt ="This template tests to make sure blocks can be replaced with markdownish syntax."
         templ = """
