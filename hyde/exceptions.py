f77743c49415cc7d67c9ef5e9706e5d521a44ff7
Kieran Wood <kieranw098@gmail.com>
2020-03-13 20:11:04 -0600

    Reverting python 3 fix & setting each python version to install

--- hyde/exceptions.py
+++ hyde/exceptions.py
@@ -1,14 +1,10 @@
+from hyde._compat import reraise
+
+
 class HydeException(Exception):
     """Base class for exceptions from hyde."""
 
-    def __init__(self, *args):
-        if args:
-            self.message = args[0]
-        else:
-            self.message = None
-    def __repr__(self):
-        """What is printed if something goes wrong"""
-        if self.message:
-            return "Exception raised: {}".format(self.message)
-        else:
-            return "Unlucky there bud"
+    @staticmethod
+    def reraise(message, exc_info):
+        _, _, tb = exc_info
+        reraise(HydeException, HydeException(message), tb)
